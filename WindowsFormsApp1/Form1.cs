f724cbe6e7b8bdbc39fabc59d44796b9b4d3e89f
Guilherme Motta <guilherme_motta@ucl.br>
2021-11-12 20:27:44 -0300

    Fazer o elevador mover ate onde o botão interno foi apertado

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -95,7 +95,40 @@ namespace Elevator
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //mover o elevador quando o botao for apertado
+            for(int n=0;n<8;n++)
+            {
+                int req = insideReq[n];
+                int pos = (int)nowFloor;
 
+                if(req !=0)// alguem chamou
+                {
+                    if(n!=pos)//não é o andar
+                    {
+                        cageGrid1[0, 7 - pos].Value = null;
+
+                        if(n>pos)//subir
+                        {
+                            pos++;
+                            cageGrid1[0, 7 - pos].Value = closeImage;
+                            nowFloor++;
+                        }
+                        else if(n<pos)//desce
+                        {
+                            pos--;
+                            cageGrid1[0, 7 - pos].Value = closeImage;
+                            nowFloor--;
+                        }
+                    }
+                    else //é o andar certo
+                    {
+                        cageGrid1[0, 7 - pos].Value = closeImage;
+                        insideReq[n] = 0;
+                        inside1.Rows[7 - pos].DefaultCellStyle.BackColor = Color.White;
+                    }
+                    break;
+                }
+            }
         }
         
         int[] insideReq = new int[8];
@@ -105,14 +138,15 @@ namespace Elevator
         String[] strFloor = new string[8] { "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8" };
         enum Floor { F1,F2,F3,F4,F5,F6,F7,F8};
         Floor nowFloor = Floor.F1; //posição inicial
+
         //botão interno
         private void inside1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             int row = inside1.CurrentRow.Index;
             insideReq[7 - row] = 7 - row + 1;
-            string str = String.Format("Inside1 Click {0}", strFloor[7 - row]);
+            // teste dos botoes string str = String.Format("Inside1 Click {0}", strFloor[7 - row]);
             inside1.Rows[row].DefaultCellStyle.BackColor = Color.Orange;
-            MessageBox.Show(str);
+            // teste dos botoes MessageBox.Show(str);
             inside1.Rows[row].DefaultCellStyle.BackColor = Color.White;
         }
 
@@ -120,9 +154,9 @@ namespace Elevator
         {
             int row = outUp.CurrentRow.Index;
             upReq[7 - row] = 7 - row + 1;
-            string str = String.Format("Up Click {0}", strFloor[7 - row]);
+            // teste dos botoes string str = String.Format("Up Click {0}", strFloor[7 - row]);
             outUp.Rows[row].DefaultCellStyle.BackColor = Color.Orange;
-            MessageBox.Show(str);
+            // teste dos botoes MessageBox.Show(str);
             outUp.Rows[row].DefaultCellStyle.BackColor = Color.White;
         }
 
@@ -130,9 +164,9 @@ namespace Elevator
         {
             int row = outDown.CurrentRow.Index;
             downReq[7 - row] = 7 - row + 1;
-            string str = String.Format("Down Click {0}", strFloor[7 - row]);
+            // teste dos botoesstring str = String.Format("Down Click {0}", strFloor[7 - row]);
             outDown.Rows[row].DefaultCellStyle.BackColor = Color.Orange;
-            MessageBox.Show(str);
+            // teste dos botoes MessageBox.Show(str);
             outDown.Rows[row].DefaultCellStyle.BackColor = Color.White;
         }
     }
