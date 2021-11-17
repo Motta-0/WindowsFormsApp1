835c15727eb99a6dce7ff6d010cfa3545b1bb9f1
Guilherme Motta <guilherme_motta@ucl.br>
2021-11-16 22:07:50 -0300

    algo de errado ainda não ta certo

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,10 +13,23 @@ namespace Elevator
     enum Floor { F1, F2, F3, F4, F5, F6, F7, F8 };
     enum Direct { STOP, UP, DOWN };
     enum Most { LOWER, UPPER };
+    enum Door { CLOSE, OPEN};
     public partial class Form1 : Form
     {
         Image closeImage = null;
         Image openImage = null;
+        int[] insideReq = new int[8];
+        int[] upReq = new int[8];
+        int[] downReq = new int[8];
+
+        String[] strFloor = new string[8] { "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8" };
+        Floor nowFloor = Floor.F1; //posição inicial
+        const int QUESIZE = 22; //prioridade no chamar do elevador
+        int[] insQue = new int[QUESIZE];
+        int reqFloor = 0; //sem nenhum chamado
+        Direct direct = Direct.STOP;
+        int passCtr = 0;
+        Door door = Door.CLOSE;
 
         public Form1()
         {
@@ -46,7 +59,7 @@ namespace Elevator
             //externo_desce
             outDown.RowTemplate.Height = 40;
             outDown.Rows.Add(8);
-            for (int n = 0; n < 7; n++)
+            for (int n = 0; n < 8; n++)
             {
                 outDown[0, n].Value = string.Format("{0}", 8 - n);
             }
@@ -85,6 +98,7 @@ namespace Elevator
             nowFloor = Floor.F1;
             reqFloor = 0;
             direct = Direct.STOP;
+            door = Door.CLOSE;
         }
 
         private void inside1_SelectionChanged(object sender, EventArgs e)
@@ -147,6 +161,7 @@ namespace Elevator
             int now = (int)nowFloor;
             if (reqFloor == 0)
                 reqFloor = popQue();
+
             if (reqFloor > 0)
             {
                 int req = reqFloor - 1; //andar que foi chamado
@@ -174,6 +189,7 @@ namespace Elevator
                     reqFloor = 0; //sem chamados
                 }
             }
+
             if(passCtr >0)
             {
                 passCtr--;
@@ -193,19 +209,6 @@ namespace Elevator
 
             }
         }
-        
-        int[] insideReq = new int[8];
-        int[] upReq = new int[8];
-        int[] downReq = new int[8];
-
-        String[] strFloor = new string[8] { "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8" };
-        Floor nowFloor = Floor.F1; //posição inicial
-        const int QUESIZE = 8; //prioridade no chamar do elevador
-        int[] insQue = new int[QUESIZE];
-        int reqFloor = 0; //sem nenhum chamado
-        Direct direct = Direct.STOP;
-        int passCtr = 0;
-
 
         //botão interno
         private void inside1_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -226,21 +229,33 @@ namespace Elevator
         private void outUp_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             int row = outUp.CurrentRow.Index;
-            upReq[7 - row] = 7 - row + 1;
-            // teste dos botoes string str = String.Format("Up Click {0}", strFloor[7 - row]);
+            int req = 7 - row + 11;
+            upReq[7 - row] = req;
+            pushQue(req);
+            ShowInsQue(req);
             outUp.Rows[row].DefaultCellStyle.BackColor = Color.Orange;
-            // teste dos botoes MessageBox.Show(str);
+            ShowUpReq(req);
+
             outUp.Rows[row].DefaultCellStyle.BackColor = Color.White;
+
+            ShowUpReq(req);
+            
+   
         }
 
         private void outDown_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             int row = outDown.CurrentRow.Index;
-            downReq[7 - row] = 7 - row + 1;
+            int req = 7 - row + 21;
+            downReq[7 - row] = req;
+            pushQue(req);
+            ShowInsQue(req);
             // teste dos botoesstring str = String.Format("Down Click {0}", strFloor[7 - row]);
             outDown.Rows[row].DefaultCellStyle.BackColor = Color.Orange;
+            ShowDownReq(req);
             // teste dos botoes MessageBox.Show(str);
             outDown.Rows[row].DefaultCellStyle.BackColor = Color.White;
+            ShowDownReq(req);
         }
 
         private void pushQue(int p)
@@ -293,7 +308,41 @@ namespace Elevator
                 {
                     DoorClose1();
                 }
-            }      
+            }
+
+            if (door == Door.CLOSE)
+            {
+                if (reqFloor == 0)
+                    reqFloor = popQue();
+                if (reqFloor > 0)
+                {
+                     now  = (int)nowFloor;
+                    int req = (reqFloor % 10) - 1;
+                    if (req != now)
+                    {
+                        if (req > now)
+                        {
+                            MoveUp1();
+                            direct = Direct.UP;
+                        }
+                        else
+                        {
+                            MoveDown1();
+                            direct = Direct.DOWN;
+                        }
+                    }
+                    else
+                    {
+                        DoorOpen1();
+                        door = Door.OPEN;
+                    }
+                }
+            }
+            else
+            {
+                DoorClose1();
+                door = Door.CLOSE;
+            }
         }
 
         private void MoveUp1()
@@ -327,6 +376,10 @@ namespace Elevator
             passCtr = 3;
             string str1 = string.Format("> Door Open at {0}", nowFloor);
             ShowListBox_1(str1);
+            clearQue(now + 1);
+            clearQueUp(now + 1);
+            clearQueDown(now + 1);
+
         }
 
         private void DoorClose1()
@@ -338,6 +391,13 @@ namespace Elevator
             reqFloor = 0; //sem chamado
             string str1 = string.Format("< Door Close at {0}", nowFloor);
             ShowListBox_1(str1);
+            inside1.Rows[7 - now].DefaultCellStyle.BackColor = Color.White;
+            if (upReq[now] == 0)
+                outUp.Rows[7 - now].DefaultCellStyle.BackColor = Color.White;
+            if(downReq[now] == 0)
+                outDown.Rows[7 - now].DefaultCellStyle.BackColor = Color.White;
+            str1 = string.Format("< Door Close at {0}", nowFloor);
+
         }
 
         private void CageUp()
@@ -345,7 +405,7 @@ namespace Elevator
             int now = (int)nowFloor;
             int req = insideReq[now];
 
-            if(req==0) // porta fechada ou passou
+            /*if(req==0) // porta fechada ou passou
             {
                 if (IsUpper1() == true)
                 {
@@ -362,6 +422,69 @@ namespace Elevator
             {
                 StopFloor1();
             }
+            else
+            {
+                DoorClose1();
+                door = Door.CLOSE;
+            }
+            */
+            if (door == Door.CLOSE)
+            {
+                // now = (int)nowFloor;
+                // req = insideReq[now];
+                if (req == 0)// porta fechada ou passou
+                {
+                    if (IsUpper1() == true)
+                    {
+                        MoveUp1();
+                        direct = Direct.UP;
+                    }
+                    else
+                    {
+                        EndWork(Most.UPPER);
+                        direct = Direct.STOP;
+                    }
+                }
+                else
+                {
+                    StopFloor1();
+                    door = Door.OPEN;
+                }
+            }
+            else
+            {
+                DoorClose1();
+                door = Door.CLOSE;
+            }
+
+            if (door == Door.CLOSE)
+            {
+                if (IsUpStop() == true)
+                {
+                    StopFloor1();
+                    door = Door.OPEN;
+                }
+                else
+                {
+                    if(IsUpper1() == true)
+                    {
+                        MoveUp1();
+                        direct = Direct.UP;
+                    }
+                    else if(IsUpperDown()==true)
+                    {
+                        StopFloor1();
+                        door = Door.OPEN;
+                    }
+                    else
+                    {
+                        EndWork(Most.UPPER);
+                        direct = Direct.STOP;
+                    }
+                }
+            }
+           
+            
         }
 
         private void StopFloor1()
@@ -374,6 +497,24 @@ namespace Elevator
             passCtr = 3;
             string str1 = string.Format("> Door Open at {0}", nowFloor);
             ShowListBox_1(str1);
+
+            if (now==0)
+            {
+                if(reqFloor==upReq[now])
+                {
+                    reqFloor = 0;
+                    upReq[now] = 0;
+                    clearQueUp(now + 1);
+                }
+                else if(now==7)
+                {
+                    if (reqFloor == downReq[now])
+                        reqFloor = 0;
+                    downReq[now] = 0;
+                    clearQueDown(now + 1);
+                }
+                passCtr = 3;
+            }
         }
 
         private bool IsUpper1()
@@ -386,6 +527,37 @@ namespace Elevator
                 {
                     flag = true;
                     break;
+                }
+            }
+            for(int n=now+1;n<8;n++)
+            {
+                if(insideReq[n]>0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(flag==false)
+            {
+                for (int n=now+1;n<7;n++)
+                {
+                    if(upReq[n] > 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if(flag==false)
+            {
+                for (int n=now+1;n<8;n++)
+                {
+                    if(downReq[n]>0)
+                    {
+                        flag = true;
+                        break;
+                    }
                 }
             }
             return flag;
@@ -405,7 +577,7 @@ namespace Elevator
         {
             int now = (int)nowFloor;
             int req = insideReq[now];
-            if (req == 0)
+            /*if (req == 0)
             {
                 if (IsLower1() ==  true)
                 {
@@ -422,6 +594,64 @@ namespace Elevator
             {
                 StopFloor1();
             }
+            */
+            if (door == Door.CLOSE)
+            {
+                //now = (int)nowFloor;
+                //req = insideReq[now];
+                if (req == 0)
+                {
+                    if (IsLower1() == true)
+                    {
+                        MoveDown1();
+                        direct = Direct.DOWN;
+                    }
+                    else
+                    {
+                        EndWork(Most.LOWER);
+                        direct = Direct.STOP;
+                    }
+                }
+                else
+                {
+                    StopFloor1();
+                    door = Door.OPEN;
+                }
+            }
+            else
+            {
+                DoorClose1();
+                door = Door.CLOSE;
+            }
+
+            if (door == Door.CLOSE)
+            {
+
+                if (IsDownStop() == true)
+                {
+                    StopFloor1();
+                    door = Door.OPEN;
+                }
+                else
+                {
+                    if (IsLower1() == true)
+                    {
+                        MoveDown1();
+                        direct = Direct.DOWN;
+                    }
+                    else if (IsLowerUp() == true)
+                    {
+                        StopFloor1();
+                        door = Door.OPEN;
+                    }
+                    else
+                    {
+                        EndWork(Most.LOWER);
+                        direct = Direct.STOP;
+                    }
+                }
+
+            }
         }
 
         private bool IsLower1()
@@ -434,6 +664,28 @@ namespace Elevator
                 {
                     flag = true;
                     break;
+                }
+            }
+            if(flag ==true)
+            {
+                for(int n=now-1;n>0;n--)
+                {
+                    if(downReq[n] > 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if (flag == false)
+            {
+                for(int n=now-1;n>=0;n--)
+                {
+                    if(upReq[n]>0)
+                    {
+                        flag = true;
+                        break;
+                    }
                 }
             }
         return flag;
@@ -453,6 +705,8 @@ namespace Elevator
                     break;
                 }
             }
+             if (p == reqFloor)
+                reqFloor = 0;
         }
 
         private void ShowListBox_1(string str1)
@@ -464,5 +718,159 @@ namespace Elevator
             }
             listBox1.SelectedIndex = listBox1.Items.Count - 1;
         }
+
+        private bool IsUpperDown()
+        {
+            int now = (int)nowFloor;
+            bool flag = false;
+            for(int n=7;n>=now;n--)
+            {
+                if(downReq[n]>0)
+                {
+                    if(n==now)
+                    {
+                        if(reqFloor==downReq[now])
+                            reqFloor = 0;
+                        downReq[now] = 0;
+                        clearQueDown(now + 1);
+                        ShowDownReq(-(now + 21));
+                        flag = true;
+                    }
+                    break;
+                }
+            }
+            return flag;
+        }
+
+        private bool IsUpStop()
+        {
+            int now = (int)nowFloor;
+            bool flag = false;
+            if((insideReq[now]>0)||(upReq[now]>0))
+            {
+                insideReq[now] = 0;
+                clearQue(now + 1);
+                upReq[now] = 0;
+                clearQueUp(now + 1);
+                ShowUpReq(-(now + 11));
+                flag = true;
+            }
+            return flag; 
+
+        }
+
+        private bool IsLowerUp()
+        {
+            int now = (int)nowFloor;
+            bool flag = false;
+            for(int i=0;i<=now;i++)
+            {
+                if(upReq[i]>0)
+                {
+                    if(i==now)
+                    {
+                        if (reqFloor == upReq[now])
+                            reqFloor = 0;
+                        upReq[now] = 0;
+                        clearQueUp(now+1);
+                        ShowUpReq(-(now + 11));
+                        flag = true;
+                    }
+                    break;
+                }
+            }
+            return flag;
+        }
+
+        private bool IsDownStop()
+        {
+            int now = (int)nowFloor;
+            bool flag = false;
+            if((insideReq[now]>0)||(downReq[now]>0))
+            {
+                insideReq[now] = 0;
+                clearQue(now + 1);
+                downReq[now] = 0;
+                clearQueDown(now + 1);
+                ShowDownReq(-(now + 21));
+                flag = true;
+            }
+            return flag;
+        }
+
+        private void ShowUpReq(int req)
+        {
+            string str1 = string.Format("U=*{0}*", req);
+            string str2 = "";
+            for (int i=0; i<8; i++)
+            {
+                str2 = upReq[i].ToString();
+                str1 += "," + str2;
+            }
+            ShowListBox_1(str1);
+        }
+
+        private void ShowDownReq(int req)
+        {
+            string str1 = string.Format("D=*{0}*", req);
+            string str2 = "";
+            for (int i = 0; i < 8; i++)
+            {
+                str2 = downReq[i].ToString();
+                str1 += "," + str2;
+            }
+            ShowListBox_1(str1);
+        }
+
+        private void ShowInsQue(int req)
+        {
+            string str1 = string.Format("<< == Push {0}=*", req);
+            string str2 = "";
+            for (int i = 0; i < 8; i++)
+            {
+                str1 += "," + str2;
+                str2 = insQue[i].ToString();
+            }
+            ShowListBox_1(str1);
+        }
+
+        private void clearQueUp(int p)
+        {
+            p += 10;
+            for (int n=0; n<QUESIZE;n++)
+            {
+                if(insQue[n] == p)
+                {
+                    for(n++;n<QUESIZE; n++)
+                    {
+                        insQue[n -1] = insQue[n];
+                    }
+                    insQue[n - 1] = 0;
+                    break;
+                }
+            }
+            if (p == reqFloor)
+                reqFloor = 0;
+        }
+
+        private void clearQueDown(int p)
+        {
+            p += 20;
+            for (int n = 0; n < QUESIZE; n++)
+            {
+                if (insQue[n] == p)
+                {
+                    for (n++; n < QUESIZE; n++)
+                    {
+                        insQue[n - 1] = insQue[n];
+                    }
+                    insQue[n - 1] = 0;
+                    break;
+                }
+            }
+            if (p == reqFloor)
+                reqFloor = 0;
+        }
+
     }
 }
